555df41fb0123892795bd9755013df4628fc7ad1
JJoustra <159259335+JJoustra@users.noreply.github.com>
2026-04-16 10:25:28 +0200

    kleine toggle func in main.c. DO NOT APPROVE

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,7 +4,10 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 }
+bool a = true;
 
 void loop() {
   //als ik hier meer dan 10 regels zie dan doe ik jullie wat aan
+  a = !a;
+  Serial.println(a);
 }
